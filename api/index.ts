c7973813996633837f08fa49329eefdbe2d65d30
piyushkumar003 <121641367+piyushkumar003@users.noreply.github.com>
2026-07-23 04:20:49 +0530

    chore: add diagnostic logs to server initialization

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,12 @@
+console.log("Loading api/index");
+
 import { createApp } from '../server/app';
+
+console.log("Imported createApp");
 
 const app = createApp();
 
+console.log("Created app");
+
 export default app;
+
